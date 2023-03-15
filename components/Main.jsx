c9ae96b0094bc452fbf7fb0d649273e6aacebd5b
robertomattus <robertmatt113@gmail.com>
2023-03-15 17:03:21 -0600

    feature: update

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
     return (
         <div id='home' className='w-full h-screen text-center'>
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
-            <div className='mt-10'>
+            <div className='mt-8'>
 
             <p className='uppercase text-sm tracking-widest text-gray-500'>
                 “To iterate is human, to recurse divine.”
